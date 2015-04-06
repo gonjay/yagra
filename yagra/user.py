2d7b92db3c4605e61d8d144e951639c11ca70600
GonJay <assyer@gmail.com>
2015-04-06 20:44:59 +0800

    Try to refacator user

--- yagra/user.py
+++ yagra/user.py
@@ -7,43 +7,72 @@ import re
 import hashlib
 from lib import db_config as config
 
+import MySQLdb as mdb
+db = mdb.connect(
+    config['host'],
+    config['user'],
+    config['password'],
+    config['db'])
+cur = db.cursor()
 
-class User():
+class User(object):
     """docstring for User"""
-    def __init__(self):
-        import MySQLdb as mdb
-        self.db = mdb.connect(
-            config['host'],
-            config['user'],
-            config['password'],
-            config['db'])
-        self.cur = self.db.cursor()
+    def __init__(self, **kwargs):
+        self.id = kwargs.get("id")
+        self.email = kwargs.get("email")
+        self.password = kwargs.get("email")
+        self.salt = kwargs.get("salt")
+        self.token = kwargs.get("token")
 
-    def query(self, sql):
-        self.cur.execute(sql)
-        row = self.cur.fetchone()
+    def query_user(sql):
+        cur.execute(sql)
+        row = cur.fetchone()
         if row is None:
             return None
         else:
-            self.id = row[0]
-            self.email = row[1]
-            self.salt = row[2]
-            self.token = row[3]
-            return self
+            user = User(
+                id=row[0], email=row[1],
+                salt=row[2], token=row[3]
+                )
+            return user
 
-    def find_by_id(self, id):
+    def insert_user(email, password):
+        strs = string.ascii_uppercase + string.digits
+        salt = ''.join(random.choice(strs) for _ in range(10))
+        token = sha.new(salt + password).hexdigest()
+        sql = "insert into user (email, salt, token) " \
+        "values ('%s', '%s', '%s')" % (email, salt, token)
+        cur.execute(sql)
+        db.commit()
+        user = User(
+            id=self.cur.lastrowid, email=email,
+            salt=salt, token=token
+            )
+        return user
+
+    def update(self):
+        strs = string.ascii_uppercase + string.digits
+        salt = ''.join(random.choice(strs) for _ in range(10))
+        token = sha.new(salt + self.password).hexdigest()
+        sql = "update users set email = '%s', salt = '%s', token = '%s'" \
+        "where id = %s" % (email, salt, token, id)
+        cur.execute(sql)
+        db.commit()
+        return self
+
+    def find_by_id(id):
         sql = "select * from user where id=%s limit 1" % id
-        return self.query(sql)
+        return query_user(sql)
 
-    def find_by_email(self, email):
+    def find_by_email(email):
         sql = "select * from user where email='%s' limit 1" % email
-        return self.query(sql)
+        return query_user(sql)
 
     def get_avatar(self):
         return hashlib.md5(self.email).hexdigest()
 
     def login(self, email, password):
-        if self.find_by_email(email):
+        if find_by_email(email):
             token = sha.new(self.salt + password).hexdigest()
             if self.token != token:
                 return None
@@ -52,25 +81,25 @@ class User():
         else:
             return None
 
-    def create(self, email, password, password_confirm):
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return "Email incorrect"
-        if len(password) < 6:
-            return "Password too short"
-        if password != password_confirm:
-            return "Twice password not match"
-        if self.find_by_email(email):
-            return "Email %s has been registered" % email
+    def create(**kwargs):
+        user = User(kwargs)
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", user.email):
+            user.errors.append("Email incorrect")
+        if len(user.password) < 6:
+            user.errors.append("Password too short")
+        if find_by_email(email):
+            user.errors.append("Email %s has been registered" % email)
+        if len(user.errors) < 1:
+            return user.save()
         else:
-            strs = string.ascii_uppercase + string.digits
-            salt = ''.join(random.choice(strs) for _ in range(10))
-            token = sha.new(salt + password).hexdigest()
-            sql = "insert into user (email, salt, token) " \
-                  "values ('%s', '%s', '%s')" % (email, salt, token)
-            self.cur.execute(sql)
-            self.db.commit()
-            self.id = self.cur.lastrowid
-            return True
+            return user
+
+    def save(self):
+        user = find_by_id(self.id)
+        if user is None:
+            return insert_user(self.email, self.password)
+        else:
+            return self.update()
 
     def save_avatar(self, fileitem):
         extension = os.path.splitext(fileitem.filename)[1]

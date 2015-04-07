56b5f36590ecff0611f15b77e7370d3264e9c0ef
GonJay <assyer@gmail.com>
2015-04-07 15:39:49 +0800

    Add routes handle

---
+++ yagra/routes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import handler.index
+import handler.session
+import handler.user
+
+handlers = [
+    (r"/", handler.index.MainHandler),
+
+    (r"/sign_in", handler.session.SessionHandler),
+    (r"/sign_up", handler.user.UserHandler),
+    (r"/logout", handler.session.SessionHandler),
+
+    (r"/user/avatar", handler.user.UserHandler),
+]

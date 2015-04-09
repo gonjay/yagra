ea973a4b8fcdb3799e90d204de297c0da83ac586
GonJay <assyer@gmail.com>
2015-04-09 16:48:12 +0800

    Implement sign in feature

--- yagra/handler/sessions.py
+++ yagra/handler/sessions.py
@@ -2,8 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from base import BaseHandler
+from ..lib.user import User
 
 class SessionHandler(BaseHandler):
 
     def get(self):
+        """Get sign in page"""
         self.render_tplates("sessions/new.html")
+
+
+    def post(self):
+        """Post email and password to login"""
+        email = self.form.getvalue('email', '')
+        password = self.form.getvalue('password', '')
+        user = User().auth(email, password)
+
+        if user is not None:
+            self.login_user(user)
+            self.redirect_to("/")
+        else:
+            html = """\
+            <html><body>
+            <h1>Password or Email incorrect</h1>
+            <a href="/sign_in">Back</a>
+            </body></html>
+            """
+            self.render(html)
+
+    def delete(self):
+        pass
